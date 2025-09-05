3f0092cd496c37d6f39c409feb7817af5e58582e
jjang051jhta <jjang051jhta@gmail.com>
2025-09-05 14:17:53 +0900

    time 문제풀이

--- day11/date/src/test/Test03.java
+++ day11/date/src/test/Test03.java
@@ -11,16 +11,23 @@ public class Test03 {
         //중간에 12:00~13:00은 휴게로 제외한다. 총 근무 시간을 분 단위로 반환하라.
         //09:00 ~ 18:800
         LocalDate today = LocalDate.now();
-        LocalDateTime start =  LocalDateTime.of(today, LocalTime.of(9,00));
+        LocalDateTime start =  LocalDateTime.of(today, LocalTime.of(9,30));
         LocalDateTime end =  LocalDateTime.of(today, LocalTime.of(18,0));
         LocalDateTime lunchStart = LocalDateTime.of(start.toLocalDate(),LocalTime.of(12,0));
         LocalDateTime lunchEnd = LocalDateTime.of(start.toLocalDate(),LocalTime.of(13,0));
-        LocalDateTime start02 = start.isBefore(lunchStart) ? start : lunchStart;
-        LocalDateTime end02 = end.isAfter(lunchEnd) ? end : lunchEnd;
-        System.out.println(Duration.between(start,end).toMinutes() - 60);
-        //System.out.println(start.isAfter(lunchStart));
-        //System.out.println(start+" / "+lunchStart);
-        //System.out.println(Duration.between(start02,end02).toMinutes());
+
+        //전체 근무시간
+        Duration total =  Duration.between(start,end);
+        System.out.println("전체 근문 시간 : "+total.toMinutes());
+
+        //겹치는 구간 연산
+        LocalDateTime overlapStart = start.isAfter(lunchStart) ? start: lunchStart;
+        LocalDateTime overlapEnd = end.isBefore(lunchEnd) ? end: lunchEnd;
+        System.out.println(overlapStart+" / "+overlapEnd);
+        Duration overlapTime = Duration.between(overlapStart,overlapEnd);
+        System.out.println("overlapTime : "+overlapTime.toMinutes());
+        System.out.println("총 근무시간 : "+total.minus(overlapTime).toMinutes());
+
 
     }
 }
